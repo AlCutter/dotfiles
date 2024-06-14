aa8e47316baff974f7b9746b713188f8d13d3f8b
Al Cutter <al@google.com>
2024-06-14 17:24:09 +0100

    Move nvim global rename

--- .config/nvim/lua/al/treesitter.lua
+++ .config/nvim/lua/al/treesitter.lua
@@ -49,7 +49,7 @@ return {
 					enable = true,
 					-- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
 					keymaps = {
-						smart_rename = "grr",
+						smart_rename = "grR",
 					},
 				},
 			},

9dcb1d0106aa976a17335fc69bc4fcbf9c06ddb4
Al Cutter <al@9600.org>
2026-09-25 10:22:18 +0100

    Hypr updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,16 +2,12 @@
 -- See https://wezfurlong.org/wezterm/
 
 local wezterm = require 'wezterm'
-
-local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.font_size = 13.0
 config.font = wezterm.font "EnvyCodeR Nerd Font Mono"
-config.macos_window_background_blur = 30
-config.window_decorations = 'RESIZE'
+-- config.window_decorations = 'RESIZE'
 config.audible_bell = "Disabled"
-config.front_end = "WebGpu"
 config.hide_tab_bar_if_only_one_tab = true
 return config
 
